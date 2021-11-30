0e5322ae083a6351f299269deab3e4ef63be01a6
Wesley Ferreira <wesleyfc00@hotmail.com>
2021-11-29 22:35:28 -0300

    Initial Commit

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native';
+import { SignIn } from './src/screens/SignIn/index';
+
+
+export default function App() {
+  return (
+      <SignIn />
+    )
+}
