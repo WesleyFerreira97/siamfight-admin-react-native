33c6f2cf8b2a3d7689088cebb66166c658472ea9
Wesley Ferreira <wesleyfc00@hotmail.com>
2021-12-03 03:02:46 -0300

    Person navbar

--- App.tsx
+++ App.tsx
@@ -1,46 +1,52 @@
 import React from 'react'
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import Icon from 'react-native-vector-icons/Feather';
 
 import { SearchClient } from './src/screens/SearchClient/index';
 import { AddClient } from './src/screens/AddClient/index';
 import { Vencimentos } from './src/screens/Vencimentos/index';
 
-const Stack = createNativeStackNavigator();
 const Tab = createMaterialBottomTabNavigator();
-
-function Tabs() {
-  return (
-    <Tab.Navigator
-      
-      >
-      <Tab.Screen name="SearchClient" component={SearchClient} />
-      <Tab.Screen name="AddClient" component={AddClient} />
-      <Tab.Screen 
-        name="Vencimentos" 
-        component={Vencimentos} 
-        options={{
-          title: 'Vencimentosaaaa',
-        }}
-        />
-    </Tab.Navigator>
-  )
-}
-
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="SearchClient">
-        <Stack.Screen 
+      <Tab.Navigator
+        initialRouteName="SearchClient"
+      >
+        <Tab.Screen 
           name="SearchClient" 
-          component={Tabs}
+          component={SearchClient} 
           options={{
             title: 'Buscar Cliente',
+            tabBarIcon: () => (
+              <Icon name="search" size={25} color="#fff" />
+            ),
           }}
         />
-      </Stack.Navigator>
+        <Tab.Screen 
+          name="AddClient" 
+          component={AddClient} 
+          options={{
+            title: 'Add Cliente',
+            tabBarIcon: () => (
+              <Icon name="user-plus" size={25} color="#fff"/>
+            ),
+          }}
+        />
+        <Tab.Screen 
+          name="Vencimentos" 
+          component={Vencimentos} 
+          options={{
+            title: 'Vencimentos',
+            tabBarIcon: () => (
+              <Icon name="dollar-sign" size={25} color="#fff"/>
+            ),
+          }}
+        />
+      </Tab.Navigator>
+
     </NavigationContainer>
     )
 }
