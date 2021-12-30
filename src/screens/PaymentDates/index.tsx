ea147120025943f525e5e8c5cb0353085bbea3d5
Wesley Ferreira <wesleyfc00@hotmail.com>
2021-12-30 03:47:23 -0300

    Screen PaymentDates - filter just active clients

--- src/screens/PaymentDates/index.tsx
+++ src/screens/PaymentDates/index.tsx
@@ -7,17 +7,38 @@ import { ListPayment } from '../../components/ListPayment';
 
 export function PaymentDates() {
   const [allClients, setAllClients] = useState<any[]>([]);
-console.log(allClients);
+  const [activeClients, setActiveClients] = useState<any>({});
+  const [dateAsc, setDateAsc] = useState<any>({});
 
   useEffect(() => {
     // Get Data from Database
     getData('cliente').then(data => { setAllClients(data as never) });
   }, []); 
+
+  useEffect(() => {
+    if(allClients.length > 0) {
+      const actives = allClients.filter(cliente => cliente.statusClient === true);
+      setActiveClients(actives);
+    }
+  }, [allClients]);
+
+  // useEffect(() => {
+  //   let newList : any = [...activeClients];
+  //   console.log(newList);
+    
+  //   // const listAsc = () => { 
+  //   //   newList.sort((firstClient: any, secondClient: any) => firstClient.name.localeCompare(secondClient.name))
+  //   //   setDateAsc(newList);
+  //   // }
+  //   // listAsc();
+  // }, [activeClients]);
+
+
   return (
     <View style={globalStyles.container} >
         <Text style={globalStyles.title}>Próximos Vencimentos</Text>
         <FlatList 
-          data={allClients}
+          data={activeClients}
           renderItem={({ item }) => <ListPayment cliente={item} />}
           keyExtractor={(item) => item.id}
         />
